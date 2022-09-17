e51eba7d6946a3cf4c69e9b465b8bbcf6d82e37c
Công Trường Trần <92919801+trancongtruong1234@users.noreply.github.com>
2022-09-18 01:22:18 +0700

    Add files via upload

--- DWH_Wrike_CongViec/Wrike_CongViec.sql
+++ DWH_Wrike_CongViec/Wrike_CongViec.sql
@@ -1,4 +1,10 @@
-﻿
+﻿drop table LoaiCongViec
+go
+CREATE TABLE LoaiCongViec (
+    LoaiCongViecID INT PRIMARY KEY,
+	LoaiCongViec NVARCHAR(50)
+);
+go
 insert into LoaiCongViec values(
 '1',N'Triển khai'),
 ('2',N'Tư vấn'),
@@ -12,6 +18,8 @@ insert into LoaiCongViec values(
 ('10',N'Xử lý công việc hàng ngày'),
 ('11',N'Công tác khác')
 go
+drop table duan
+go
 CREATE TABLE DuAn(
 	WorkID NVARCHAR(100) PRIMARY KEY,
 	MaVuViec NVARCHAR(100),
@@ -21,10 +29,16 @@ CREATE TABLE DuAn(
 	PhongKD NVARCHAR(100), 
 	LoaiDuAn NVARCHAR(100),
 	HangSanXuat NVARCHAR(300),
-	Mau NVARCHAR(100)
+	Mau NVARCHAR(100),
+	LoaiCongViecID INT,
+	Status NVARCHAR(100),
+	Component NVARCHAR(100),
+	NgayBatDau DATE,
+	NgayDenHan DATE
 )
 go
-
+drop table [date]
+go
 CREATE TABLE [Date] (
     [NgayThucHien] date primary key,
     [Ngay] int,
@@ -33,6 +47,8 @@ CREATE TABLE [Date] (
     [Nam] int
 )
 
+go
+drop table [nhanvien]
 go
 CREATE TABLE NhanVien(
 	NhanVienID INT PRIMARY KEY,
@@ -45,27 +61,29 @@ CREATE TABLE NhanVien(
 )
 
 go
+drop  table [Timelog_Category]
+go
 Create table[Timelog_Category](
 timelog_category_id nvarchar(50) primary key,
 timelog_category_name nvarchar(100)
 )
 go
-
-CREATE TABLE LoaiCongViec (
-    LoaiCongViecID INT PRIMARY KEY,
-	LoaiCongViec NVARCHAR(50)
-);
+drop table LoaiHieuQua
+go
 CREATE TABLE LoaiHieuQua(
 	LoaiHieuQuaID INT PRIMARY KEY,
 	LoaiHieuQua NVARCHAR(50)
 )
+drop table CongViec
+go
 CREATE TABLE CongViec(
 	CongViecID INT PRIMARY KEY,
 	WorkID NVARCHAR(100),
+	ProjectID NVARCHAR(100),
 	MoTa NVARCHAR(500),
 	NgayThucHien DATE,
 	SoGio FLOAT,
-	NgayCuoiTuan INT,--trong
+	NgayCuoiTuan INT,
 	GioBatDau DATETIME,--trong
 	GioKetThuc DATETIME,--trong
 	DiaDiem NVARCHAR(500),-- empty
@@ -77,12 +95,16 @@ CREATE TABLE CongViec(
 	LoaiHieuQuaID INT,--trong
 	CategoryID nvarchar(50)
 )
+CREATE table DuAn_Component (
+WorkID nvarchar(50),
+Component nvarchar(200)
+)
 
 -- test
 truncate table congviec
 drop table CongViec
 
-
+truncate table duan_component
 truncate table congviec
 truncate table [date]
 truncate table duan
@@ -91,6 +113,7 @@ truncate table loaihieuqua
 truncate table nhanvien
 truncate table timelog_category
 
+select * from  duan_component
 Select * from DuAn
 Select * from [date]
 Select * from nhanvien
@@ -104,3 +127,36 @@ TO DISK = 'D:\DB_Goc_Wrike.bak';
 
 --790914963
 --2355459
+
+Select Sum(SoGio),nv.TenNhanVien,Thang,Nam
+from CongViec cv, Date d, NhanVien nv
+where cv.NgayThucHien=d.NgayThucHien and nv.NhanVienID=cv.NhanVienID
+group by Thang, nv.TenNhanVien,Nam
+
+
+Select * from NhanVien order by TenNhanVien
+go
+select * from duan order by workid
+go
+select count(*) as sl, workID, component from duan group by workID, component
+go
+select * from congviec
+go
+select count (workID) as SoLuong, workID from DuAn_Component 
+group by WorkID having count (workID) >1
+go
+select * from DuAn_Component where WorkID = '790927563'
+go
+SELECT t.WorkID,
+         Component = STUFF((SELECT  ','+cast(Component AS VARCHAR ) FROM  DuAn_Component WHERE  WorkID  = t.WorkID  FOR XML PATH('')),1,1,'')
+FROM DuAn_Component t
+GROUP BY t.WorkID
+go
+select
+    workID,
+    string_agg(Component,',') component
+from DuAn_Component
+group by WorkID
+
+select count (workID) as SoLuong, workID from CongViec 
+group by WorkID having count (workID) >1
